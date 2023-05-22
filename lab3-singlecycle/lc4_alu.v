9421d09c63d22230c90f96b7b7e100fa9f378533
yujiapang <123439506+yujiapang@users.noreply.github.com>
2023-05-22 12:28:36 +0800

    Lab 3 code complete

--- lab3-singlecycle/lc4_alu.v
+++ lab3-singlecycle/lc4_alu.v
@@ -10,6 +10,100 @@ module lc4_alu(input  wire [15:0] i_insn,
                output wire [15:0] o_result);
 
 
+      wire [15:0] result_0001,result_0010;
+      
+      wire [15:0] o_remainder;
+      wire [15:0] result_0101;
+      wire [15:0] result_1010;
+      wire [15:0] cla_inp0,cla_inp1,sum,cla_inp1_0010;
+      wire cin;
+      wire [3:0] shiftby=i_insn[3:0];
+      //for 1010
+      wire [15:0] s_r1=$signed(i_r1data[15:0])>>>i_insn[3:0] ;
+      
+        assign result_1010 = (i_insn[5:4] == 2'b00) ? i_r1data<<i_insn[3:0] :
+                              (i_insn[5:4] == 2'b01) ? s_r1:
+                              (i_insn[5:4] == 2'b10) ? i_r1data>>i_insn[3:0] :
+                              o_remainder;
+      
+
+      assign cla_inp1_0010 = (i_insn[8:7] == 2'b00) ? ~i_r2data :
+                  (i_insn[8:7] == 2'b01) ? ~i_r2data :
+                  (i_insn[8:7] == 2'b10) ? ~{5'b0,i_insn[6:0]} :
+                  ~{5'b0,i_insn[6:0]};
+
+     
+      assign result_0101 = (i_insn[5:3] == 3'b000) ? (i_r1data & i_r2data) :
+      (i_insn[5:3] == 3'b001) ? ( ~i_r1data ) :
+      (i_insn[5:3] == 3'b010) ? (i_r1data | i_r2data) :
+      (i_insn[5:3] == 3'b011) ? (i_r1data ^ i_r2data) :
+      (i_r1data & {{12{i_insn[4]}}, i_insn[3:0]});  
+      wire [15:0] o_quotient;
+      lc4_divider d(.i_dividend(i_r1data),.i_divisor(i_r2data),.o_remainder(o_remainder),.o_quotient(o_quotient));
+      assign result_0001 = (i_insn[5:3] == 3'b001) ? (i_r2data * i_r1data) :
+      (i_insn[5:3] == 3'b011) ? (o_quotient) :
+      (sum);
+
+    assign result_0010 = (i_insn[8]==0)?((i_insn[7]==1) ? ((i_r1data < i_r2data) ? 16'b1111111111111111 : 
+    ((i_r1data == i_r2data) ? 16'b0000000000000000 : 16'b0000000000000001)) : 
+    ((i_r1data[15]==0) ? ((i_r2data[15]==1) ? 16'b0000000000000001 : 
+    ((i_r1data < i_r2data) ? 16'b1111111111111111 : 
+    ((i_r1data == i_r2data) ? 16'b0000000000000000 : 16'b0000000000000001))) : 
+    ((i_r2data[15]==0) ? 16'b1111111111111111 : ((i_r1data < i_r2data) ? 16'b1111111111111111 : 
+    ((i_r1data == i_r2data) ? 16'b0000000000000000 : 16'b0000000000000001))))):
+    //immediate
+    ((i_insn[7]==1)?((i_r1data < {{9{1'b0}},i_insn[6:0]}) ? 16'b1111111111111111 : 
+    (i_r1data == {{9{1'b0}},i_insn[6:0]}) ? 16'b0000000000000000 : 16'b0000000000000001):
+    ((i_r1data[15]==0) ? ((i_insn[6]==1) ? 16'b0000000000000001 : 
+    ((i_r1data < {{10{i_insn[6]}},i_insn[5:0]}) ? 16'b1111111111111111 : 
+    ((i_r1data == {{10{i_insn[6]}},i_insn[5:0]}) ? 16'b0000000000000000 : 16'b0000000000000001))) : 
+    ((i_insn[6]==0) ? 16'b1111111111111111 : ((i_r1data < {{10{i_insn[6]}},i_insn[5:0]}) ? 16'b1111111111111111 : 
+    ((i_r1data == {{10{i_insn[6]}},i_insn[5:0]}) ? 16'b0000000000000000 : 16'b0000000000000001))))
+    );
+      
+assign o_result = (i_insn[15:12] == 4'b0000) ? sum :
+                  (i_insn[15:12] == 4'b0001) ? result_0001 :
+                  (i_insn[15:12] == 4'b0010) ? result_0010 :
+                  (i_insn[15:12] == 4'b0100) ? (i_insn[11] == 0) ? i_r1data : ((i_pc & 16'h8000) | (i_insn[10:0] << 4)) :
+                  (i_insn[15:12] == 4'b0101) ? result_0101 :
+                  (i_insn[15:12] == 4'b0110) ? sum :
+                  (i_insn[15:12] == 4'b0111) ? sum :
+                  (i_insn[15:12] == 4'b1000) ?  i_r1data:
+                  (i_insn[15:12] == 4'b1001) ? {{8{i_insn[8]}},i_insn[7:0]} :
+                  (i_insn[15:12] == 4'b1010) ? result_1010 :
+                  (i_insn[15:12] == 4'b1100) ? (i_insn[11] == 0) ? i_r1data : sum :
+                  (i_insn[15:12] == 4'b1101) ? (i_r1data & 8'hff) | (i_insn[7:0] << 8) :
+                  (i_insn[15:12] == 4'b1111) ? 16'h8000 | i_insn[7:0] :
+                  0;
+
+assign cla_inp0 = (i_insn[15:12] == 4'b0000) ? i_pc :
+                  (i_insn[15:12] == 4'b0001) ? i_r1data :
+                  (i_insn[15:12] == 4'b0010) ? i_r1data :
+                  (i_insn[15:12] == 4'b0110) ? i_r1data :
+                  (i_insn[15:12] == 4'b0111) ? i_r1data :
+                  (i_insn[15:12] == 4'b1010) ? 15'd15 :
+                  (i_insn[15:12] == 4'b1100) ? i_pc :
+                  0;
+
+assign cin = (i_insn[15:12] == 4'b0000) ? 1'b1 :
+            (i_insn[15:12] == 4'b0001) ? ((i_insn[5] == 0)?((i_insn[4] == 1)? 1'b1:1'b0):1'b0) :
+            (i_insn[15:12] == 4'b0010) ? 1'b1 :
+             (i_insn[15:12] == 4'b1100) ? 1'b1 :
+             (i_insn[15:12] == 4'b1010) ? 1'b1 :
+             1'b0;
+
+assign cla_inp1 = (i_insn[15:12] == 4'b0000) ?  {{8{i_insn[8]}}, i_insn[7:0]} :
+                  (i_insn[15:12] == 4'b0001) ? ((i_insn[5] == 0) ? ((i_insn[4] == 0) ? i_r2data : ~i_r2data):{{12{i_insn[4]}},i_insn[3:0]}):
+                  (i_insn[15:12] ==4'b0010) ?cla_inp1_0010:
+                  (i_insn[15:12] ==4'b0110) ?({{11{i_insn[5]}},i_insn[4:0]}):
+                  (i_insn[15:12] ==4'b0111) ?({{11{i_insn[5]}},i_insn[4:0]}):
+                  (i_insn[15:12] == 4'b1010) ? ~i_insn[3:0] :
+                  (i_insn[15:12] ==4'b1100) ?{{6{i_insn[10]}},i_insn[9:0]}:16'b0;
+
+     //assign cla
+      cla16 s(.a(cla_inp0),.b(cla_inp1),.cin(cin),.sum(sum));
+
+
       /*** YOUR CODE HERE ***/
 
 endmodule
--- lab3-singlecycle/lc4_single.v
+++ lab3-singlecycle/lc4_single.v
@@ -1,4 +1,6 @@
-/* TODO: name and PennKeys of all group members here
+/* TODO: Yaxue Liu and Yujia Pang
+   pennid:
+   maxiliu and yujiap
  *
  * lc4_single.v
  * Implements a single-cycle data path
@@ -9,6 +11,8 @@
 
 // disable implicit wire declaration
 `default_nettype none
+
+
 
 module lc4_processor
    (input  wire        clk,                // Main clock
@@ -48,28 +52,65 @@ module lc4_processor
    // disconnected ports. Feel free to use this for debugging input/output if
    // you desire.
    assign led_data = switch_data;
+   assign test_stall=2'b0;
+   wire [2:0] r1sel,r2sel,wsel,next_nzp,nzp;
+   wire r1re,r2re, regfile_we,nzp_we,select_pc_plus_one,is_load,is_store,is_branch,is_control_insn;
+   wire [15:0] o_result,o_rs_data,o_rt_data,i_wdata;
+   
+   
+   wire [15:0]   pc;      // Current program counter (read out from pc_reg)
+   wire [15:0]   next_pc,  pc_plus_one; // Next program counter (you compute this and feed it into next_pc)
+   Nbit_reg #(16, 16'h8200) pc_reg (.in(next_pc), .out(pc), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   
+   
+   
+   assign test_cur_pc=pc;
+   
+   assign next_pc = (is_control_insn) ? o_result :
+                  (is_branch ? (((i_cur_insn[9]==1'b1 && nzp[0]==1'b1)||((i_cur_insn[10]==1'b1 && nzp[1]==1'b1))||((i_cur_insn[11]==1'b1 && nzp[2]==1'b1))) ? o_result :
+                                 pc_plus_one) : pc_plus_one);
+
+   /*NZP logic*/
+   Nbit_reg #(3,3'b000) nzp_reg (.in(next_nzp), .out(nzp), .clk(clk), .we(nzp_we), .gwe(gwe), .rst(rst));
+    
+   assign next_nzp = (i_wdata[15] == 1'b1) ? 3'b100 : ((i_wdata == 16'b0) ? 3'b010 : 3'b001);
 
    
-   /* DO NOT MODIFY THIS CODE */
-   // Always execute one instruction each cycle (test_stall will get used in your pipelined processor)
-   assign test_stall = 2'b0; 
-
-   // pc wires attached to the PC register's ports
-   wire [15:0]   pc;      // Current program counter (read out from pc_reg)
-   wire [15:0]   next_pc; // Next program counter (you compute this and feed it into next_pc)
-
-   // Program counter register, starts at 8200h at bootup
-   Nbit_reg #(16, 16'h8200) pc_reg (.in(next_pc), .out(pc), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-
-   /* END DO NOT MODIFY THIS CODE */
-
-
+   
    /*******************************
     * TODO: INSERT YOUR CODE HERE *
     *******************************/
+lc4_decoder decode( .insn(i_cur_insn), .r1sel(r1sel), .r1re(r1re), .r2sel(r2sel),  .r2re(r2re),               // does this instruction read from rt?
+                   .wsel(wsel), .regfile_we(regfile_we), .nzp_we(nzp_we),  .select_pc_plus_one(select_pc_plus_one), // write PC+1 to the regfile?
+                   .is_load(is_load), .is_store(is_store), .is_branch(is_branch),          // is this a branch instruction?
+                   .is_control_insn(is_control_insn)     
+                   );
+   lc4_alu alu(.i_insn(i_cur_insn),.i_pc(pc),.i_r1data(o_rs_data), .i_r2data(o_rt_data),
+               .o_result(o_result));
+   lc4_regfile #(16) reg1(.clk(clk), .gwe(gwe),.rst(rst),.i_rs(r1sel), .o_rs_data(o_rs_data), // rs contents
+    .i_rt(r2sel),   .o_rt_data(o_rt_data), .i_rd(wsel),  .i_wdata(i_wdata),   // data to write
+    .i_rd_we(regfile_we)    // write enable
+    );    
 
+    cla16 s(.a(pc),.b(16'b0),.cin(1'b1),.sum(pc_plus_one));
 
-
+   assign i_wdata=(select_pc_plus_one)?(pc_plus_one):((is_load)? i_cur_dmem_data: o_result);
+   assign o_cur_pc=pc;
+   assign o_dmem_we = is_store;
+   assign o_dmem_addr=(is_load|is_store)?o_result:16'b0;
+   assign o_dmem_towrite=(is_store)? o_rt_data:16'b0;
+   assign test_cur_insn=i_cur_insn;
+   assign test_regfile_we=regfile_we;
+   assign test_regfile_wsel=wsel;
+   assign test_regfile_data=i_wdata;
+   assign test_nzp_we=nzp_we;
+   assign test_nzp_new_bits=next_nzp;
+   assign test_dmem_we=o_dmem_we;
+   assign test_dmem_addr=o_dmem_addr;
+   assign test_dmem_data=(is_load)?i_cur_dmem_data:o_dmem_towrite;    
+      
+   
+   
    /* Add $display(...) calls in the always block below to
     * print out debug information at the end of every cycle.
     *
@@ -85,8 +126,16 @@ module lc4_processor
     * when we run the grading scripts.
     */
 `ifndef NDEBUG
+   //integer printed=0;
+   
    always @(posedge gwe) begin
-      // $display("%d %h %h %h %h %h", $time, f_pc, d_pc, e_pc, m_pc, test_cur_pc);
+
+      //  if (test_cur_pc>4'h8200) begin
+      //     $display("instru: %b", i_cur_insn);end
+      //    printed = 1;
+         
+      // end
+      //$display("%d %h ", $time,  test_cur_pc);
       // if (o_dmem_we)
       //   $display("%d STORE %h <= %h", $time, o_dmem_addr, o_dmem_towrite);
 
